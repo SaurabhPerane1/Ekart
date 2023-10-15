e65ab782208a5e6553cefe4f267583ded92b4a17
SaurabhPerane1 <119008712+SaurabhPerane1@users.noreply.github.com>
2023-10-15 18:50:08 +0530

    Create keyboard1.js

---
+++ keyboard1.js
@@ -0,0 +1,114 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Logitech Keyboard',
+        image: 'k1.PNG',
+        price: 1599
+    },
+    {
+        id: 2,
+        name: 'Logi Keyboard',
+        image: 'k2.PNG',
+        price: 1199
+    },
+    {
+        id: 3,
+        name: 'Gigabyte Keyboard',
+        image: 'k3.PNG',
+        price: 1099
+    },
+    {
+        id: 4,
+        name: 'Gaming Keyboard rgb',
+        image: 'k4.PNG',
+        price: 1249
+    },
+    {
+        id: 5,
+        name: 'A4 TECH Keyboard',
+        image: 'k5.PNG',
+        price: 1999
+    },
+    {
+        id: 6,
+        name: 'Zebronics Keyboard',
+        image: 'k6.WEBP',
+        price: 999
+    },
+    {
+        id: 7,
+        name: 'Gigabyte Keyboard',
+        image: 'k3.PNG',
+        price: 999
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
